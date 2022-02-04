a841e204bf106a90b46aa6b7036cd30041a4b186
Best-Verie <irabestverie2016@gmail.com>
2022-02-04 22:20:53 +0200

    display state data

--- src/Quiz.js
+++ src/Quiz.js
@@ -5,12 +5,14 @@ class Quiz extends Component {
 
     constructor(props){
         super(props)
-        this.state = {quiz_position : 1}
+        this.state =  quizData.quiz_questions[0]
+        console.log("stateeeeeeeeee",this.state)
     }
     render(){
         return (
             <div className="QuizQuestion">
-                
+                {this.state.instruction_text}
+                {/* {this.state.quiz_position[0].instruction_text} */}
             </div>
         )
     }
